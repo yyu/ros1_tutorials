a413ca930623efdb8d2f91d7949fdcadf5dc2751
y² <yy@yuyuan.org>
2018-09-10 04:14:50 +0000

    catkin_make install

---
+++ catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/src/beginner_tutorials/msg")
+set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
--- catkin_ws/install/share/beginner_tutorials/cmake/beginner_tutorialsConfig.cmake
+++ catkin_ws/install/share/beginner_tutorials/cmake/beginner_tutorialsConfig.cmake
@@ -66,7 +66,7 @@ endif()
 set(beginner_tutorials_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
-if("TRUE" STREQUAL "TRUE")
+if("FALSE" STREQUAL "TRUE")
   set(beginner_tutorials_SOURCE_PREFIX /home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/src/beginner_tutorials)
   set(beginner_tutorials_DEVEL_PREFIX /home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/devel)
   set(beginner_tutorials_INSTALL_PREFIX "")
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(beginner_tutorials_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT "/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/devel/include " STREQUAL " ")
+if(NOT "include " STREQUAL " ")
   set(beginner_tutorials_INCLUDE_DIRS "")
-  set(_include_dirs "/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/devel/include")
+  set(_include_dirs "include")
   if(NOT " " STREQUAL " ")
     set(_report "Check the issue tracker '' and consider creating a ticket if the problem has not been reported yet.")
   elseif(NOT "http://wiki.ros.org/ROS/Tutorials/CreatingPackage " STREQUAL " ")
@@ -110,7 +110,7 @@ if(NOT "/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/devel/include " STREQ
         message(FATAL_ERROR "Project 'beginner_tutorials' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'beginner_tutorials' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/src/beginner_tutorials/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'beginner_tutorials' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/install/${idir}'.  ${_report}")
     endif()
     _list_append_unique(beginner_tutorials_INCLUDE_DIRS ${include})
   endforeach()
@@ -129,7 +129,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/devel/lib;/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/devel/lib;/opt/ros/melodic/lib)
+    foreach(path /home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/install/lib;/home/ubuntu/__a__/github/ros1_tutorials/catkin_ws/devel/lib;/opt/ros/melodic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
